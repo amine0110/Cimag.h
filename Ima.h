c07f78edd6eba8b9cd3cd32eea42f84d494c1637
Mokhtari Mohammed El Amine <37108394+amine0110@users.noreply.github.com>
2020-01-10 23:23:19 +0100

    Update Ima.h

--- Ima.h
+++ Ima.h
@@ -7,14 +7,17 @@ class CIma
 public:
 	CIma();
 	~CIma();
-	//**************
+	
 	byte clip(int a);
 	void ConvertRGBYUY2(BYTE *ptrIn, BYTE *pyuv, int Larg, int Haut);
 	void ConvertYUY2RGB(BYTE *PtrIn, BYTE *ptrOut, int Larg, int Haut);
-	void Contour(unsigned char * ImrgbPtr, int Larg, int Haut);
-	void Contour2(unsigned char * ImrgbPtr, int Larg, int Haut);
-	void inversion(unsigned char *ImrgbPtr, int Larg, int Haut);
-	void seuillage(unsigned char *ImrgbPtr, int Larg, int Haut);
+	void Contour(unsigned char * ImagTrait, int Larg, int Haut);
+	void Contour2(unsigned char * ImagTrait, int Larg, int Haut);
+	void inversion(unsigned char *ImagTrait, int Larg, int Haut);
+	void seuillage(unsigned char *ImagTrait, int Larg, int Haut);
+	void rotation(unsigned char *ImagTrait, int Larg, int Haut);
+	void CIma::Flou(unsigned char * ImrgbPtr, int Larg, int Haut);
+	
 
 
 };
